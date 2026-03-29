81eb490cf533b554ffbc440fb26be79c52c4c5cc
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2026-03-29 16:43:24 -0400

    feat: implementa Formulario de Busca com validacao

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+import Form from "./Form";
 import Navigation from "./Navigation";
 
 function Header({handleSubmit}) {
   return (
     <header>
       <h1>Photo Gallery</h1>
+      <Form />
       <Navigation />
     </header>
   );
