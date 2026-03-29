1685ef2cea499ac0762f90f3730f6ce4258769fd
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2026-03-29 15:14:44 -0400

    feat: mapeia rotas estaticas para as categorias Islands, Architecture, Coding e Coffee

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ function Navigation() {
   return (
     <nav>
       <ul>
-        <li><NavLink to="/island">Island</NavLink></li>
+        <li><NavLink to="/islands">Islands</NavLink></li>
         <li><NavLink to="/architecture">Architecture</NavLink></li>
         <li><NavLink to="/coding">Coding</NavLink></li>
         <li><NavLink to="/coffee">Coffee</NavLink></li>
