a9b3aad86b5b6ad094edbc80f5ace5954dc1c7e1
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2026-03-29 14:36:12 -0400

    feat: cria funcao runSearch com Axios para buscar fotos na API do Flickr

--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -1,5 +1,40 @@
-function PhotoContextProvider() {
+import { createContext, useState } from "react";
+import { apiKey } from "../api/Config";
+import axios from "axios";
+
+export const PhotoContext = createContext();
+
+function PhotoContextProvider(props) {
+
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function runSearch(query) {
+    
+    setLoading(true);
+    setImages([]);
+
+    try {
+      const response = await axios.get(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${query}&format=json&nojsoncallback=1`
+      );
+      
+      setImages(response.data.photos.photo);
+
+    } catch (error) {
+      console.error('Erro ao buscar imagens:', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <PhotoContext.Provider value={{ images, loading, runSearch }}>
+        {props.children}
+      </PhotoContext.Provider>
+    </>
+  );
 
 }
-
 export default PhotoContextProvider;
