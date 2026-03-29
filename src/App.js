7f2692b42f4451a52c2c1f0c452ad30e88fa4494
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2026-03-29 15:16:24 -0400

    feat: configura roteamento principal e estrutura base da interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import PhotoContextProvider from './context/PhotoContext';
 import Header from './components/Header';
+import Item from './components/Item';
 
 function App() {
   return (
@@ -10,7 +11,14 @@ function App() {
         <Header />
 
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          {/* Rota Inicial */}
+          <Route path="/" element={<Navigate to="/Islands" replace />} />
+
+          {/* Rotas para as categorias */}
+          <Route path="/Islands" element={<Item searchItem="Islands" />} />
+          <Route path="/Architecture" element={<Item searchItem="Architecture" />} />
+          <Route path="/Coding" element={<Item searchItem="Coding" />} />
+          <Route path="/Coffee" element={<Item searchItem="Coffee" />} />
         </Routes>
       </BrowserRouter>
     </PhotoContextProvider>
